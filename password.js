6a09a4731594bf829e2798ea45a3de5252e53d04
Henry Fitch <crazydude23365@gmail.com>
2019-12-05 09:28:50 -0500

    Saved work for today. Will fix later

---
+++ password.js
@@ -0,0 +1,23 @@
+var IsCorrect = false;
+
+var SoftLocked = false;
+var password;
+
+var tries = 0;
+while (IsCorrect == false) {
+
+password = prompt("Enter password(It's 12345):");
+
+if (password == 12345 && SoftLocked == false) {
+alert("ACCESS GRANTED");
+IsCorrect = true;
+alert("WELCOME, USER.");
+} else {
+alert("ACCESS DENIED");
+tries = tries + 1;
+if (tries == 3 || SoftLocked == true) {
+alert ("THREE ATTEMPTS MADE. PROGRAM SOFTLOCKED.");
+SoftLocked = true;
+}
+}
+}
